3e6f3fea38f6ad323b923852d752db248a927c0f
github username <jhernandez1692@gmail.com>
2020-09-26 16:17:41 -0400

    set up function blueprint.

--- projectMain/Project1.cpp
+++ projectMain/Project1.cpp
@@ -5,3 +5,82 @@ using namespace std;
 
 // Jeffer Hernandez - 15007776
 // CSC 231 -0500
+
+// CSC231 Discrete Structure and Applications
+// Programming Assignment
+// Counting the Number of Days between January 1, 1900 to today (whenever the program is run)
+// Using Virtual Studio that is installed on the Virtual Machine, write a program that computes and displays
+// the number of days that have elapsed between January 1, 1900 and today (the day the program is run).
+
+// The program must be designed as follows:
+// Within a class named elapsed_days, provide the following functions:
+
+// • bool is_year_valid(long year)
+// returns true if year is >= 1900; otherwise false
+
+// • bool is_month_valid(long month)
+// returns true if month is between 1 and 12 (inclusive); otherwise false
+
+// • bool is_leap_year(long year)
+// returns true if year is a leap year
+
+// • long return_days_in_month(long year, long month)
+// returns the number of days for the month month in year year
+
+// • long return days_in_year(year)
+// returns the number of days in year
+
+// • void set_elapsed_days(long elapsed_days)
+// sets the field value set_elapsed_days
+
+// • long get_elapsed_days()
+// returns the field value set_elapsed_days
+
+// • void compute_elapsed days()
+// returns the number of days elapsed between January 1, 1900 and the day the program is run
+
+// • void display_elapsed_days()
+// displays the number of days elapsed in the form “The number of days elapsed
+// since Jan 1, 1900 is xxxx.” Where xxxx is the number of the elapsed days
+
+// • A constructor that determines the number of elapsed days
+
+// • A suitable destructor
+
+
+// Within a main(…) driver function:
+// • Instantiates an instance of elapsed_days
+// • Displays the number of elapsed days
+
+bool is_year_valid(long year){}
+
+bool is_month_valid(long month){}
+
+bool is_leap_year(long year){}
+
+long return_days_in_month(long year, long month){}
+
+long return_days_in_year(long year){}
+
+void set_elapsed_days(long elapsed_days){}
+
+long get_elapsed_days(){}
+
+void compute_elapsed_days(){}
+
+void display_elapsed_days(){}
+
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+return 0;
+}
